4a8dcda49951dc4464b92aa4298b43b845281885
Julián Andrés Escobar Herrera <julian.escobar@ipalmera.co>
2019-08-30 09:35:28 -0500

    *Se ocultan errores de debug credomatic

--- app/code/Pago/Credomatic/Model/Credomatic.php
+++ app/code/Pago/Credomatic/Model/Credomatic.php
@@ -1,9 +1,9 @@
 <?php 
 namespace Pago\Credomatic\Model;
 
-ini_set('display_errors', 1);
+/*ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ALL);*/
 
 /*
 *Clase Credomatic:
